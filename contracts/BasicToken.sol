f7c8ec515c205e8e5a3ef31d7f4585d2a3eea883
Kostya Kulyk <kostya.kulyk@seagate.com>
2018-01-15 16:08:10 +0200

    Cosmetic changes

--- contracts/BasicToken.sol
+++ contracts/BasicToken.sol
@@ -20,8 +20,6 @@ contract BasicToken is ERC20Basic {
     */
     function transfer(address _to, uint256 _value) public returns (bool) {
         require(_to != address(0));
-        address sender = msg.sender;
-        uint256 balance = balances[msg.sender];
         require(_value <= balances[msg.sender]);
 
         // SafeMath.sub will throw if there is not enough balance.
